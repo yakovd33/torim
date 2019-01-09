960e790b1a0586b91f424d655bde48a9eb4f4ba8
Yakov Shitrit <yakovd33@gmail.com>
2019-01-09 18:22:06 +0200

    Search page template added

---
+++ search.php
@@ -0,0 +1,204 @@
+<?php include 'includes/header.php'; ?>
+
+<link rel="stylesheet" href="assets/css/search.css">
+
+<div class="site-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 text-right" id="search-filter-wrap">
+                <div id="category-filter-wrap">
+                    <a class="category-filter-item active" href="#">כל הקטגוריות <div class="results-counter">119</div></a>
+                    <a class="category-filter-item" href="#">מספרות <div class="results-counter">87</div></a>
+                    <a class="category-filter-item" href="#">וטרינרים <div class="results-counter">28</div></a>
+                    <a class="category-filter-item" href="#">בתי קפה <div class="results-counter">17</div></a>
+                    <a class="category-filter-item" href="#">מרפאות <div class="results-counter">14</div></a>
+                    <a class="category-filter-item" href="#">מוסדות ממשלתיים <div class="results-counter">3</div></a>
+                </div>
+
+                <div id="checkboxes-filters">
+                    <input id='one' type='checkbox'/>
+                    <label for='one' dir="ltr">
+                        <span></span>
+                        אפשרות לתשלום באתר
+                        <ins><i>אפשרות לתשלום באתר</i></ins>
+                    </label>
+                </div>
+            </div>
+
+            <div class="col-md-9 text-right" id="search-results-wrap">
+                <div id="search-form-content">
+                    <input class="form-control" id="form-input" type="search" placeholder="חיפוש" aria-label="Search">
+                    <button class="btn btn-outline-info  my-2 my-sm-0 hvr-buzz-out" id="form-button" type="submit">חיפוש</button>
+                </div>
+                
+                <div id="number-of-results">מציג <strong>119</strong> תוצאות חיפוש</div>
+
+                <div id="search-results">
+                    <div class="search-result-card">
+                        <div class="hot-search-result">
+                            <img src="assets/photos/flame.png ">
+                        </div>
+
+                        <div class="search-result-dets">
+                            <div class="search-result-thumbnail">
+                                <img src="https://previews-themeforest.imgix.net/files/237003152/BookYourTravel-WordPress-Theme-Preview.png?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=f7c6eb04e85ba1ee27f3481356bd6d37">
+                                <div class="search-result-tags">
+                                    <span class="search-result-tags-title">תגיות:</span> <a href="#" class="search-result-tag">מספרה</a>, <a href="#" class="search-result-tag">מספרת נשים</a>, <a href="#" class="search-result-tag">עיצוב שיער</a>
+                                </div>
+                            </div>
+
+                            <div class="search-result-textual">
+                                <p class="search-result-title">מספרת שיער שיערי</p>
+                                <p class="search-result-company">מיקי בוגנים</p>
+
+                                <p class="search-result-description">
+                                    לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית מוסן מנת. להאמית קרהשק סכעיט דז מא, מנכם למטכין נשואי מנורך. ושבעגט ליבם סולגק.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="search-result-info">
+                            <i class="fas fa-heart like save-booking"></i>
+
+                            <div class="rate">
+                                <div class="stars" data-stars="1">
+                                    <svg height="25" width="23" class="star rating" data-rating="1">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="2">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="3">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="4">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="5">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                </div>
+                            </div>
+
+                            <div class="opening-hours">
+                                <strong>שעות פתיחה:</strong> <br>
+                                א'-ה': 9:00-18:00 <br>
+                                ו': 9:00-13:00
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="search-result-card">
+                        <div class="hot-search-result">
+                            <img src="assets/photos/flame.png ">
+                        </div>
+
+                        <div class="search-result-dets">
+                            <div class="search-result-thumbnail">
+                                <img src="https://previews-themeforest.imgix.net/files/237003152/BookYourTravel-WordPress-Theme-Preview.png?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=f7c6eb04e85ba1ee27f3481356bd6d37">
+                                <div class="search-result-tags">
+                                    <span class="search-result-tags-title">תגיות:</span> <a href="#" class="search-result-tag">מספרה</a>, <a href="#" class="search-result-tag">מספרת נשים</a>, <a href="#" class="search-result-tag">עיצוב שיער</a>
+                                </div>
+                            </div>
+
+                            <div class="search-result-textual">
+                                <p class="search-result-title">מספרת שיער שיערי</p>
+                                <p class="search-result-company">מיקי בוגנים</p>
+
+                                <p class="search-result-description">
+                                    לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית מוסן מנת. להאמית קרהשק סכעיט דז מא, מנכם למטכין נשואי מנורך. ושבעגט ליבם סולגק.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="search-result-info">
+                            <i class="fas fa-heart like save-booking"></i>
+
+                            <div class="rate">
+                                <div class="stars" data-stars="1">
+                                    <svg height="25" width="23" class="star rating" data-rating="1">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="2">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="3">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="4">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="5">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                </div>
+                            </div>
+
+                            <div class="opening-hours">
+                                <strong>שעות פתיחה:</strong> <br>
+                                א'-ה': 9:00-18:00 <br>
+                                ו': 9:00-13:00
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="search-result-card">
+                        <div class="hot-search-result">
+                            <img src="assets/photos/flame.png ">
+                        </div>
+
+                        <div class="search-result-dets">
+                            <div class="search-result-thumbnail">
+                                <img src="https://previews-themeforest.imgix.net/files/237003152/BookYourTravel-WordPress-Theme-Preview.png?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=f7c6eb04e85ba1ee27f3481356bd6d37">
+                                <div class="search-result-tags">
+                                    <span class="search-result-tags-title">תגיות:</span> <a href="#" class="search-result-tag">מספרה</a>, <a href="#" class="search-result-tag">מספרת נשים</a>, <a href="#" class="search-result-tag">עיצוב שיער</a>
+                                </div>
+                            </div>
+
+                            <div class="search-result-textual">
+                                <p class="search-result-title">מספרת שיער שיערי</p>
+                                <p class="search-result-company">מיקי בוגנים</p>
+
+                                <p class="search-result-description">
+                                    לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית מוסן מנת. להאמית קרהשק סכעיט דז מא, מנכם למטכין נשואי מנורך. ושבעגט ליבם סולגק.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="search-result-info">
+                            <i class="fas fa-heart like save-booking"></i>
+
+                            <div class="rate">
+                                <div class="stars" data-stars="1">
+                                    <svg height="25" width="23" class="star rating" data-rating="1">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="2">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="3">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="4">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                    <svg height="25" width="23" class="star rating" data-rating="5">
+                                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"></polygon>
+                                    </svg>
+                                </div>
+                            </div>
+
+                            <div class="opening-hours">
+                                <strong>שעות פתיחה:</strong> <br>
+                                א'-ה': 9:00-18:00 <br>
+                                ו': 9:00-13:00
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
