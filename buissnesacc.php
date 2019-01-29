ef532865ca3bd861df2a0be6d57a79e66a7641d1
Nave Shitrit <naveshitrit79@gmail.com>
2019-01-29 22:54:50 +0200

    commit

--- buissnesacc.php
+++ buissnesacc.php
@@ -1,74 +1,58 @@
 <?php include 'includes/header.php'; ?>
 
-<link rel="stylesheet" href="assets/css/account.css">
 
+<head>
+
+<link rel="stylesheet" href="assets/css/buissnesacc.css">
+
+</head>
+
+
+<body>
+    
 <div class="site-wrap">
     <div id="container">
-        <ul class="tabs">
-            <li class="tab-link current" data-tab="tab-1">התחברות</li>
-            <li class="tab-link" data-tab="tab-2">הרשמה</li>
-        </ul>
-
-        <div id="tab-1" class="login tab-content current">
-
-            <form>
-                <div class="form-section">
-                    <span class="fas fa-envelope input-icon"></span>
-                    <input type="email" name="email" placeholder="אימייל">
-                </div>
-
-                <div class="form-section">
-                    <span class="fa fa-unlock-alt input-icon"></span>
-                    <input type="password" name="password" placeholder="סיסמא">
-                </div>
-
-                <div class="form-section">
-                    <span class="fas fa-code input-icon"></span>
-                    <input type="text" name="text" placeholder="קוד זיהוי">
-                </div>
-
-                <div class="form-section btn-container">
-                    <input type="submit" value="התחברות">
-                </div>
-            </form>
-
-        </div>
-            
-        <div id="tab-2" class="register tab-content">
-                <form>
-                    <div class="form-section">
-                        <span class="fas fa-user input-icon"></span>
-                        <input type="text" name="name" placeholder="שם העסק" class="שם העסק">
-                    </div>
-
-                    <div class="form-section">
-                        <span class="fas fa-envelope input-icon"></span>
-                        <input type="email" name="name" placeholder="אימייל" class="אימייל">
-                    </div>
-
-                    <div class="form-section">
-                        <span class="fa fa-unlock-alt input-icon"></span>
-                        <input type="password" name="password" placeholder="סיסמא">
-                    </div>
-
-                    <div class="form-section">
-                        <span class="fa fa-unlock-alt input-icon"></span>
-                        <input type="password" name="re-password" placeholder="אימות סיסמא">
-                    </div>
-
-                    <div class="form-section">
-                        <span class="fas fa-code input-icon"></span>
-                        <input type="text" name="text" placeholder="קוד זיהוי">
-                    </div>
-
-                    <div class="form-section btn-container">
-                        <input type="submit" value="הרשמה">
-                    </div>
-                </form>
-            </div>
-        </div>
+         <!-- multistep form -->
+<form id="msform">
+  <!-- progressbar -->
+  <ul id="progressbar">
+    <li class="active">Account Setup</li>
+    <li>Social Profiles</li>
+    <li>Personal Details</li>
+  </ul>
+  <!-- fieldsets -->
+  <fieldset>
+    <h2 class="fs-title">Create your account</h2>
+    <h3 class="fs-subtitle">This is step 1</h3>
+    <input type="text" name="email" placeholder="Email" />
+    <input type="password" name="pass" placeholder="Password" />
+    <input type="password" name="cpass" placeholder="Confirm Password" />
+    <input type="button" name="next" class="next action-button" value="Next" />
+  </fieldset>
+  <fieldset>
+    <h2 class="fs-title">Social Profiles</h2>
+    <h3 class="fs-subtitle">Your presence on the social network</h3>
+    <input type="text" name="twitter" placeholder="Twitter" />
+    <input type="text" name="facebook" placeholder="Facebook" />
+    <input type="text" name="gplus" placeholder="Google Plus" />
+    <input type="button" name="previous" class="previous action-button" value="Previous" />
+    <input type="button" name="next" class="next action-button" value="Next" />
+  </fieldset>
+  <fieldset>
+    <h2 class="fs-title">Personal Details</h2>
+    <h3 class="fs-subtitle">We will never sell it</h3>
+    <input type="text" name="fname" placeholder="First Name" />
+    <input type="text" name="lname" placeholder="Last Name" />
+    <input type="text" name="phone" placeholder="Phone" />
+    <textarea name="address" placeholder="Address"></textarea>
+    <input type="button" name="previous" class="previous action-button" value="Previous" />
+    <input type="submit" name="submit" class="submit action-button" value="Submit" />
+  </fieldset>
+</form>
     </div>
-    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
 
     <script>
         /* Creator of this Jquery https://codepen.io/cssjockey/pen/jGzuK */
@@ -85,4 +69,6 @@
             })
     </script>
 
-<?php include 'includes/footer.php'; ?>
+    <script src="assets/js/buissnesacc.js"></script>
+
+</body>
