eace2a25d7664ca89b5232c1e880bb1175e01d73
Nave Shitrit <naveshitrit79@gmail.com>
2020-06-06 17:59:20 +0300

    company page

---
+++ company.php
@@ -0,0 +1,73 @@
+<?php include 'includes/header.php'; ?>
+
+<link rel="stylesheet" href="assets/css/company.css">
+
+
+<div id="company-profile" class="container-fluid">
+
+    <div class="row">
+
+        <div class="col-md-3" id="profile-right">
+
+            <div id="company-logo-wrap">
+                <img src="assets\photos\clalit-logo.png" id="company-logo">
+            </div>
+
+            <h2 id="company-name"> קופת חולים כללית</h2>
+
+            <h3 id="company-job">קופת חולים</h3>
+
+            <div id="company-details">
+
+                <div>
+                    <h4 class="details-headlines">שעות פעילות</h4>
+                    <p>08:00 - 18:00</p>
+                </div>
+
+                <div>
+                    <h4 class="details-headlines">קצת על העסק</h4>
+                    <p>אני קצת טקסט על העסק</p>
+                </div>
+
+                <div>
+                    <h4 class="details-headlines">מיקום</h4>
+                    <p>אני יודע איפה אתה נמצא יבן זונה</p>
+                </div>
+
+            </div>
+
+            <div class="rate">
+                <div class="stars" data-stars="1">
+                    <svg height="25" width="23" class="star rating" data-rating="1">
+                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"/>
+                    </svg>
+                    <svg height="25" width="23" class="star rating" data-rating="2">
+                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"/>
+                    </svg>
+                    <svg height="25" width="23" class="star rating" data-rating="3">
+                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"/>
+                    </svg>
+                    <svg height="25" width="23" class="star rating" data-rating="4">
+                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"/>
+                    </svg>
+                    <svg height="25" width="23" class="star rating" data-rating="5">
+                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"/>
+                    </svg>
+                </div>
+            </div>
+
+            <!-- <div id="buttons">
+                <button>אודות</button>
+                <button>ביקורות</button>
+                <button>צור קשר</button>
+            </div> -->
+
+        </div>
+
+    </div>
+
+</div>
+
+<?php include 'includes/footer.php'; ?>
+
+<!--/en-in/registration/-->
