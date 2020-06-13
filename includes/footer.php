c143508b6fa27f11a80d177294f807d6a3c5906a
נווה שטרית <nave@MacBook-Pro.local>
2020-06-13 21:54:42 +0300

    latest

--- includes/footer.php
+++ includes/footer.php
@@ -146,7 +146,7 @@
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-6 col-12 text-right">
                 © 2018 כל הזכויות שמורות
-                <a href="#"> מנתור.com</a>
+                <a href="#"> הזמנתור.com</a>
             </div>
 
             <div class="col-lg-6 col-md-6 col-sm-6 col-12 text-left">
